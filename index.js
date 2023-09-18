41a74dca13075fa7d34f792ad67fad69429336da
Velma Kimani <stanvelma16@gmail.com>
2023-09-18 15:37:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(freeRide){
+if(freeRide <= 400){
+  return "This one is on me!";
+  
+}else if(freeRide > 400 && freeRide < 2500){
+  return "I will gladly take your thirty bucks.";
+}else{
+  return "No can do.";
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+}
+else{
+    return "No go.";
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+//   let tip = "generous";
+//  let thankyouMessage;
+//   switch(tip){
+//     case tip = "generous" :
+//       thankyouMessage = "Thank you so much.";
+//      break;
+//     case tip = "not as generous" :
+//       thankyouMessage = "Thank you.";
+//      break;
+//     default : 
+//     thankyouMessage = "Bye.";
+//    break;
+//   }
+//   return thankyouMessage;
+  if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
+}
+
 }
